1f5f33eb381df465faedb0b5e9aff77d73cbf0af
Seb <everydayseb@pm.me>
2026-05-03 02:13:35 +0100

    Add code to board

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,17 @@
 # Represents the game board
 class Board
+  attr_reader :code
+
   BOARD_SIZE = 12
   def initialize
     @board = array.new(Board::BOARD_SIZE)
   end
+
+  def store_code(code)
+    self.code = code
+  end
+
+  private
+
+  attr_writer :code
 end
