062c5e20743549b2cb349eb6f93cf330d764fecd
Seb <everydayseb@pm.me>
2026-05-03 02:09:35 +0100

    Add create_code to computer player

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -3,4 +3,11 @@ class ComputerPlayer
   def initialize(gameboard)
     @board = gameboard
   end
+
+  def create_code
+    code = Array.new(4)
+    random = Random.new
+    code.each_index { |index| code[index] = random.rand(1..6) }
+    code
+  end
 end
