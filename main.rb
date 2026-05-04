d133c69a4aa744431bfe625d626e745140aa7e72
Seb <everydayseb@pm.me>
2026-05-04 17:15:35 +0100

    Add menu for choosing role

--- main.rb
+++ main.rb
@@ -3,8 +3,20 @@ require_relative 'lib/computer_player'
 require_relative 'lib/human_player'
 
 board = Board.new
-codemaker = ComputerPlayer.new(board)
-codebreaker = HumanPlayer.new(board)
+
+puts 'Who do you want to play as?'
+puts '1 - Codemaker'
+puts '2 - Codebreaker'
+response = gets.chomp
+case response
+when '1'
+  codemaker = HumanPlayer.new(board)
+  codebreaker = ComputerPlayer.new(board)
+when '2'
+  codemaker = ComputerPlayer.new(board)
+  codebreaker = HumanPlayer.new(board)
+end
+
 board.players = { codemaker: codemaker, codebreaker: codebreaker }
 
 board.store_code(codemaker.create_code)
