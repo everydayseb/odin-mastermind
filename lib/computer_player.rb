f3d836a32f7e67dba246e1b1a6bdf86322a34d0c
Seb <everydayseb@pm.me>
2026-05-05 23:31:59 +0100

    Shuffle the candidates list

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -51,7 +51,7 @@ class ComputerPlayer
   attr_accessor :board, :code, :canditates
 
   def create_candidates
-    [1, 2, 3, 4, 5, 6].repeated_permutation(4).to_a
+    [1, 2, 3, 4, 5, 6].repeated_permutation(4).to_a.shuffle
   end
 
   def feedback_matches?(last_guess, candidate, last_feedback)
